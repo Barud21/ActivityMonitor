065def86a81c9b14300246908484fe0f79ea928b
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-02-16 21:26:44 +0100

    Sample json 2020_02_16.json

--- GUI_Python.py
+++ GUI_Python.py
@@ -1,6 +1,18 @@
 import wx
 
-app = wx.App()
-frame = wx.Frame(parent=None, title='Hello World')
-frame.Show()
-app.MainLoop()
+class MyFrame(wx.Frame):
+    def __init__(self):
+        super().__init__(parent=None, title='Activity Monitor')
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        self.text_ctrl = wx.TextCtrl(panel)
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)
+        my_btn = wx.Button(panel, label='Press Me')
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)
+        panel.SetSizer(my_sizer)
+        self.Show()
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
