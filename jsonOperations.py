4c84476926b92e05513fd91424f3709d4663927a
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-02-19 22:25:38 +0100

    Updated folder with date files, jsonOperations.py fixed to suit up the new format of json files.

--- jsonOperations.py
+++ jsonOperations.py
@@ -1,20 +1,21 @@
 import json
+import time
 from _datetime import datetime
 from operator import itemgetter
 
 
 # loads json data to memory
 def defLoadDate():
-    fileName = datetime.today().strftime('%Y_%m_%d') + ".json"          # opening file with today's date by default
-    with open(fileName, 'r') as read_date:                              # opening file with today's date
-        dateData = json.load(read_date)                                 # loading data to memory
-    return dateData                                                     # returning data
+    fileName = datetime.today().strftime('JSON_files/%Y_%m_%d') + ".json"       # opening file with today's date by default
+    with open(fileName, 'r') as read_date:                                      # opening file with today's date
+        dateData = json.load(read_date)                                         # loading data to memory
+    return dateData                                                             # returning data
 
 
 # iterates through whole json file
 def defIterateJson(jsonData):
-    for instance in range(len(jsonData)):                               # loop through every major object in list
-        print(json.dumps(jsonData[instance], indent=4))                 # printing up the data from json
+    for k, v in jsonData.items():       # loop through every major object in list
+        print(k, v)                     # printing up the data from json
         print('=' * 50)
     return
 
@@ -23,29 +24,42 @@ def defIterateJson(jsonData):
 # and total time is a value, then it sorts the list in descending order
 def defSummingUpTotalTime(jsonData):
     totalTimeForApplication = {}
-    for dataNumber in range(len(jsonData)):                                                                     # iterating through major applications
-        timeTotal = 0                                                                                           # defining total time variable
-        applicationKey = list(jsonData[dataNumber].keys())                                                      # finding key (application name), which is different for every object
-        for instance in range(len(jsonData[dataNumber][applicationKey[0]]["instances"])):                       # iterating through instances in every application
-            timeTotal += jsonData[dataNumber][applicationKey[0]]["instances"][instance]["total"]                # summimg up time from every instance
-        print("Total time value for {0} instance is {1}".format(dataNumber, timeTotal))
-        totalTimeForApplication[applicationKey[0]] = timeTotal                                                  # creating dictionary, where applications are keys and values are total usage times
-    totalTimeForApplication = sorted(totalTimeForApplication.items(), key=itemgetter(1), reverse=True)          # sorting dictionary in descending order
+    totalTimeForAllApps = 0
+    for keyNumber in range(len(jsonData)):                                                      # iterating through major applications
+        timeTotal = 0                                                                           # defining total time variable
+        applicationKey = list(jsonData.keys())                                                  # finding key (application name), which is different for every object
+        for instance in range(len(jsonData[applicationKey[keyNumber]])):                        # iterating through instances in every application
+            timeTotal += jsonData[applicationKey[keyNumber]][instance]["total"]                 # summimg up time from every instance
+        totalTimeForAllApps += timeTotal                                                        # summing up total usage time for all apps
+        totalTimeForApplication[applicationKey[keyNumber]] = {}                                 # creating dictionary, where applications are keys and values are total usage times
+        totalTimeForApplication[applicationKey[keyNumber]]['Usage time'] = timeTotal
+    for keyNumber in range(len(totalTimeForApplication)):
+        applicationKey = list(totalTimeForApplication.keys())
+        timePercentage = round(totalTimeForApplication[applicationKey[keyNumber]]['Usage time'] / totalTimeForAllApps, 2)
+        totalTimeForApplication[applicationKey[keyNumber]]['Percentage time of use'] = timePercentage
+    totalTimeForApplication = sorted(totalTimeForApplication.items(), key=lambda x: x[1]["Usage time"],
+                                     reverse=True)  # sorting dictionary in descending order
+    # timeTotal = time.strftime('%Hh %Mm %Ss', time.gmtime(timeTotal))      może zmienić w format czasu w liście a może nie
     print(totalTimeForApplication)
+    print('=' * 50)
     return totalTimeForApplication
 
 
 def defSortingInstances(jsonData):
-    for dataNumber in range(len(jsonData)):
-        applicationKey = list(jsonData[dataNumber].keys())
-        jsonData[dataNumber][applicationKey[0]]["instances"] = \
-            sorted(jsonData[dataNumber][applicationKey[0]]["instances"], key=lambda x: x["total"],reverse=True)
+    for keyNumber in range(len(jsonData)):
+        applicationKey = list(jsonData.keys())
+        jsonData[applicationKey[keyNumber]] = sorted(jsonData[applicationKey[keyNumber]], key=lambda x: x["total"],reverse=True)
     print(jsonData)
     return jsonData
 
+def defPrintingInstances(jsonData, instanceName):
+    for instance in range(len(jsonData[instanceName])):
+        print(jsonData[instanceName][instance])
 
 data = defLoadDate()
 totalUsageTimeForApplication = defSummingUpTotalTime(jsonData=data)
 data = defSortingInstances(data)
-
-$ pip freeze > requirements_br.txt
+instance = ''
+instance = input("Please give the name of application ")
+defPrintingInstances(data, instanceName=instance)
+defIterateJson(data)
