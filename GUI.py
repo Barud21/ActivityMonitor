eae0fef564276c8fa8d21b8a1955f92dd3397c17
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-03-21 22:45:00 +0100

    Rewritting jsonOperations and GUI.

--- GUI.py
+++ GUI.py
@@ -1,29 +1,59 @@
-try:
-    import tkinter as tk
-except ImportError: # python 2
-    import Tkinter as tk
-
+import tkinter as tk
 import matplotlib.figure
 import matplotlib.patches
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import os
-from jsonOperations import defLoadDate
-from jsonOperations import defSummingUpTotalTime
+import time
+import jsonOperations as jO
 
-data = defLoadDate()
-summedTime = defSummingUpTotalTime(jsonData=data)
-house_prices = [summedTime[0][1]["Percentage time of use"], summedTime[1][1]["Percentage time of use"] , summedTime[2][1]["Percentage time of use"]]
+window = tk.Tk()
+window.title('Activity Monitor')
+
+window.columnconfigure(0, weight=1000)
+window.columnconfigure(1, weight=500)
+window.columnconfigure(2, weight=1)
+window.columnconfigure(3, weight=500)
+window.columnconfigure(4, weight=1)
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
+window.rowconfigure(1, weight=1)
+label = tk.Label(window, text = "Hello World")
+label.grid(row=0, column=0)
+
+data = jO.defDecodingJson()
+summedTime = jO.defSummingUpTotalTime(data)
+percentageTime = jO.defPercentageCalculation(summedTime)
+sortedInstances = jO.defSortedInstances(data)
+
+house_prices = percentageTime
 
 fig = matplotlib.figure.Figure(figsize=(5,5))
 ax = fig.add_subplot(111)
-ax.pie([summedTime[0][1]["Percentage time of use"],summedTime[1][1]["Percentage time of use"],summedTime[2][1]["Percentage time of use"]])
-ax.legend([summedTime[0][0],summedTime[1][0],summedTime[2][0]])
+ax.pie(percentageTime)
+ax.legend([x[0] for x in summedTime]) # summedTime[0][0]
 
-#circle=matplotlib.patches.Circle( (0,0), 0.7, color='white')
-#ax.add_artist(circle)
-
-window= tk.Tk()
 canvas = FigureCanvasTkAgg(fig, master=window)
-canvas.get_tk_widget().pack()
+canvas.get_tk_widget().grid(row=1, column=0)
 canvas.draw()
+
+applicationList = tk.Listbox(window)
+applicationList.grid(row=1, column=1, sticky='nsew')
+applicationList.config(border=2, relief='sunken')
+for zone in summedTime:
+    applicationList.insert(tk.END, zone[0] + ' - ' + time.strftime('%H:%M:%S', time.gmtime(zone[1])))
+
+applicationScroll = tk.Scrollbar(window, orient=tk.VERTICAL, command=applicationList.yview)
+applicationScroll.grid(row=1, column=2, sticky='nsw')
+applicationList['yscrollcommand'] = applicationScroll.set
+
+instancesList = tk.Listbox(window)
+instancesList.grid(row=1, column=3, sticky='nsew')
+instancesList.config(border=2, relief='sunken')
+for key in sortedInstances.keys():
+    for x in range(len(sortedInstances[key])):
+        instancesList.insert(tk.END, sortedInstances[key][x][0] + ' - ' + time.strftime('%H:%M:%S', time.gmtime(sortedInstances[key][x][1])))
+
+instancesScroll = tk.Scrollbar(window, orient=tk.VERTICAL, command=instancesList.yview)
+instancesScroll.grid(row=1, column=4, sticky='nsw')
+instancesList['yscrollcommand'] = instancesScroll.set
+
 window.mainloop()
--- jsonOperations.py
+++ jsonOperations.py
@@ -1,67 +1,55 @@
 import json
-import time
 from _datetime import datetime
-from operator import itemgetter
+import jsonFormatter as jF
 
 
-# loads json data to memory
-def defLoadDate():
-    fileName = datetime.today().strftime('JSON_files/%Y_%m_%d') + ".json"       # opening file with today's date by default
-    with open(fileName, 'r') as read_date:                                      # opening file with today's date
-        dateData = json.load(read_date)                                         # loading data to memory
-    return dateData                                                             # returning data
+def defDecodingJson():
+    fileName = datetime.today().strftime('JSON_files/%Y_%m_%d') + ".json"
 
-
-# iterates through whole json file
-def defIterateJson(jsonData):
-    for k, v in jsonData.items():       # loop through every major object in list
-        print(k, v)                     # printing up the data from json
-        print('=' * 50)
-    return
-
-
-# function that sums up total usage time for every application, creates dictionary, where applications are keys
-# and total time is a value, then it sorts the list in descending order
-def defSummingUpTotalTime(jsonData):
-    totalTimeForApplication = {}
-    totalTimeForAllApps = 0
-    for keyNumber in range(len(jsonData)):                                                      # iterating through major applications
-        timeTotal = 0                                                                           # defining total time variable
-        applicationKey = list(jsonData.keys())                                                  # finding key (application name), which is different for every object
-        for instance in range(len(jsonData[applicationKey[keyNumber]])):                        # iterating through instances in every application
-            timeTotal += jsonData[applicationKey[keyNumber]][instance]["total"]                 # summimg up time from every instance
-        totalTimeForAllApps += timeTotal                                                        # summing up total usage time for all apps
-        totalTimeForApplication[applicationKey[keyNumber]] = {}                                 # creating dictionary, where applications are keys and values are total usage times
-        totalTimeForApplication[applicationKey[keyNumber]]['Usage time'] = timeTotal
-    for keyNumber in range(len(totalTimeForApplication)):
-        applicationKey = list(totalTimeForApplication.keys())
-        timePercentage = round(totalTimeForApplication[applicationKey[keyNumber]]['Usage time'] / totalTimeForAllApps * 100, 2)
-        totalTimeForApplication[applicationKey[keyNumber]]['Percentage time of use'] = timePercentage
-    totalTimeForApplication = sorted(totalTimeForApplication.items(), key=lambda x: x[1]["Usage time"],
-                                     reverse=True)  # sorting dictionary in descending order
-    # timeTotal = time.strftime('%Hh %Mm %Ss', time.gmtime(timeTotal))      może zmienić w format czasu w liście a może nie
-    #print(totalTimeForApplication)
-    #print('=' * 50)
-    return totalTimeForApplication
-
-
-def defSortingInstances(jsonData):
-    for keyNumber in range(len(jsonData)):
-        applicationKey = list(jsonData.keys())
-        jsonData[applicationKey[keyNumber]] = sorted(jsonData[applicationKey[keyNumber]], key=lambda x: x["total"],reverse=True)
+    with open(fileName, 'r') as read_date:                              # opening file with today's date
+        jsonData = json.load(read_date, cls=jF.CustomJsonDecoder)       # loading data to memory
     print(jsonData)
     return jsonData
 
-def defPrintingInstances(jsonData, instanceName):
-    for instance in range(len(jsonData[instanceName])):
-        print(jsonData[instanceName][instance])
 
-data = defLoadDate()
-totalUsageTimeForApplication = defSummingUpTotalTime(jsonData=data)
-print()
-print(totalUsageTimeForApplication[0][1]['Percentage time of use'])
-print()
-data = defSortingInstances(data)
+# function that sums up total usage time for every applications and sorts application in descending order by total usage time
+def defSummingUpTotalTime(applicationList):
+    totalTimeForApplications = {}
+
+    for application in applicationList:
+        totalTimeForApplications[application.appName] = application.sumOfTotalTimeForApplication()
+
+    print(totalTimeForApplications)
+    totalTimeForApplications = sorted(totalTimeForApplications.items(), key=lambda x: x[1], reverse= True)
+    print(totalTimeForApplications)
+
+    return totalTimeForApplications
+
+# function that calculates percentage time of usage for every application
+def defPercentageCalculation(totalTimeForApplications):
+    percentageUsage = list()
+    totalUsageTime = 0
+
+    for app in totalTimeForApplications:
+        totalUsageTime += app[1]
+
+    for app in totalTimeForApplications:
+        percentageUsage.append(round(app[1] / totalUsageTime * 100))
+
+    return percentageUsage
 
 
-defIterateJson(data)
+def defSortedInstances(applicationList):
+    sortedInstances = {}
+
+    for application in applicationList:
+        sortedInstances[application.appName] = {}
+        for instance in range(len(application.instances)):
+            sortedInstances[application.appName][application.instances[instance].detailedName] = application.instances[instance].totalTime
+        sortedInstances[application.appName] = sorted(sortedInstances[application.appName].items(), key=lambda x: x[1], reverse=True)
+        print(sortedInstances)
+
+    print(sortedInstances)
+
+    return sortedInstances
+
